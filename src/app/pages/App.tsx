fa5ec2a1f58ac82e5e08e951d2ff4673f62238b5
evanmichaelstrand <evanmstrand@gmail.com>
2023-10-18 11:33:03 -0600

    get entities call working

--- src/app/pages/App.tsx
+++ src/app/pages/App.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { AppHeader, AppName, Page, Flex, Code, Heading, Paragraph, FormField, TextInput } from "@dynatrace/strato-components-preview";
 //import { getEntities } from "../functions/getEntities";
+import { monitoredEntitiesClient } from "@dynatrace-sdk/client-classic-environment-v2";
 
 
 //TODO: Add scopes to api, then assign api to get entities call
-//const data = await monitoredEntitiesClient.getEntities();
-//console.log(data);
+
+var count = 1;
+
+//https://italonascimento.github.io/applying-a-timeout-to-your-promises/
+
+
+
+const config: Object = {
+  entitySelector: "type(HOST)",
+  from: "now-365d",
+  to: "now",
+  fields: "-fromRelationships",
+}
+
+monitoredEntitiesClient.getEntities(config)
+.then((response) => {
+  response.entities?.forEach(entity => {
+    console.log(entity.displayName);
+  })
+})
+.catch((response) => {
+  console.log("error:", response)
+  count = 0;
+})
 
 export const App = () => {
   return (
     <>
     <Flex flexDirection="column" alignItems="center" padding={32}>
+
         <Heading>Dynatrace Non-Named Biz Event Impact Analysis App</Heading>
         <Paragraph>
-        This app is designed to assist a new App developer better understand the basics of the application 
+        This app is designed to assist a new App developer better understand the basics of the application {count}
         </Paragraph>
         <Paragraph>To run the app please enter an email address below</Paragraph>
         <FormField label="">
