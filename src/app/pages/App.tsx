0b930e5a4c470195510a55866c922ecea6af094d
evanmichaelstrand <evanmstrand@gmail.com>
2023-11-02 10:16:00 -0600

    working example of the datatable, next step is to programmatically add data from api to the table

--- src/app/pages/App.tsx
+++ src/app/pages/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppHeader, AppName, Page, Flex, Code, Heading, Paragraph, FormField, TextInput } from "@dynatrace/strato-components-preview";
 //import { getEntities } from "../functions/getEntities";
 import { monitoredEntitiesClient } from "@dynatrace-sdk/client-classic-environment-v2";
+import { DataTable, TableColumn } from '@dynatrace/strato-components-preview/tables';
 
 
 //TODO: Add scopes to api, then assign api to get entities call
@@ -10,13 +11,25 @@ var count = 1;
 
 //https://italonascimento.github.io/applying-a-timeout-to-your-promises/
 
+/*const entity = {
+  name: string;
+} */
+
+/*interface apiData{
+  data:Object[]
+}*/
+
+var apiData: {host: string, traffic: number, memoryTotal: number, timestamp: number}[] = [];
+
+apiData.push({"host": "test", "traffic": 213.4, "memoryTotal": 100, "timestamp": 99})
+console.log(apiData)
 
 
 const config: Object = {
   entitySelector: "type(HOST)",
   from: "now-365d",
   to: "now",
-  fields: "-fromRelationships",
+  fields: "-fromRelationships, +properties.ipAddress",
 }
 
 monitoredEntitiesClient.getEntities(config)
@@ -27,8 +40,80 @@ monitoredEntitiesClient.getEntities(config)
 })
 .catch((response) => {
   console.log("error:", response)
-  count = 0;
 })
+
+const cols: TableColumn[] = [
+  {
+    header: 'Host Info',
+    id: 'hostid',
+    columns: [
+      {
+        header: 'Host',
+        accessor: 'host',
+        minWidth: 150,
+      },
+      {
+        header: 'Timestamp',
+        accessor: 'timestamp',
+        minWidth: 200,
+      },
+    ],
+  },
+  {
+    header: 'Performance Information',
+    id: 'performanceInfo',
+    columns: [
+      {
+        header: 'Traffic',
+        accessor: 'traffic',
+        autoWidth: true,
+      },
+      {
+        header: 'Memory Total',
+        accessor: 'memoryTotal',
+        autoWidth: true,
+      },
+    ],
+  },
+];
+
+const data = [
+  {
+    host: 'et-demo-2-win4',
+    traffic: '213.4',
+    memoryTotal: 5830000000,
+    timestamp: '2022-09-26T12:45:07Z',
+    price: 290,
+  },
+  {
+    host: 'et-demo-2-win3',
+    traffic: '374',
+    memoryTotal: 3520000000,
+    timestamp: '2022-09-27T14:10:02Z',
+    price: 324,
+  },
+  {
+    host: 'et-demo-2-win1',
+    traffic: '625',
+    memoryTotal: 4670000000,
+    timestamp: '2022-09-27T13:10:02Z',
+    price: 343,
+  },
+  {
+    host: 'et-demo-2-win8',
+    traffic: '98.7',
+    memoryTotal: 5820000000,
+    timestamp: '2022-09-28T11:29:10Z',
+    price: 289,
+  },
+  {
+    host: 'dev-demo-5-macOS',
+    traffic: '164.6',
+    memoryTotal: 3460000000,
+    timestamp: '2022-09-28T10:22:56Z',
+    price: 193,
+  },
+];
 
 export const App = () => {
   return (
@@ -43,6 +128,7 @@ export const App = () => {
         <FormField label="">
           <TextInput placeholder="john.lagona@dtinside.com" />
         </FormField>
+        <DataTable columns={cols} data={apiData}/>
       </Flex>
    </>
   );
